a63ad31c19a6f8e2e8b5c0654315e75c9940b25f
Galen Sanford <galen@galen-sanfords-u-100.local>
2009-03-12 18:56:56 +0900

    Added AuthLogic

---
+++ db/migrate/20090312085803_create_users.rb
@@ -0,0 +1,23 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :login
+      t.string :crypted_password
+      t.string :password_salt
+      t.string :website
+      t.string :email_address
+      t.text :profile
+      t.boolean :active, :default => 1
+      t.string :persistence_token
+      t.integer :login_count
+      t.datetime :last_request_at
+      t.datetime :last_login_at
+      t.datetime :current_login_at
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
