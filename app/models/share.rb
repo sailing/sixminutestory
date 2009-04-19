970b5642eba3ec22b9a50ed8281030b81930029f
Galen <galen@sharearchy.com>
2009-04-19 23:42:21 +0900

    Turning off Ferret

--- app/models/share.rb
+++ app/models/share.rb
@@ -1,6 +1,6 @@
 class Share < ActiveRecord::Base
   belongs_to :user
   # Ferret full text search
-  acts_as_ferret :fields =>{:title => {:store => true},:description => {:store => true}}
+ # acts_as_ferret :fields =>{:title => {:store => true},:description => {:store => true}}
   
   end
