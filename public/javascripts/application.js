8fae91047652b934456da9df474f20e3f29f4357
Galen <galen@sharearchy.com>
2009-06-06 19:50:43 +0900

    Fixing analytics

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,2 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-9195656-1");
-pageTracker._trackPageview();
-} catch(err) {}</script>
