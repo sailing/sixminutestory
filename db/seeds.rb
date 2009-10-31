45ef79732845993e00378bfde6e25f08ac5ae1d8
sailing <galen@sharearchy.com>
2009-10-31 09:46:13 +0900

    messing more with seeds and sphinx

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   Major.create(:name => 'Daley', :city => cities.first)
 
 User.create([{:login => 'Galen', :password => 'pass', :admin_level => 3}])
-User.create([{:login => 'User', :crypted_password => '476aac1e2b50cff5651b70de3bbdbba35301dd0215625993201f5e086cc36eb6cf9ae549e75b7a1c4324687337045d9a6728d5ecc56261965bccf6dd710eac22', :password_salt => '6ZUPqL4ig8fxAz1zfMSd', :admin_level => '1}'])
+User.create([{:login => 'User', :crypted_password => '476aac1e2b50cff5651b70de3bbdbba35301dd0215625993201f5e086cc36eb6cf9ae549e75b7a1c4324687337045d9a6728d5ecc56261965bccf6dd710eac22', :password_salt => '6ZUPqL4ig8fxAz1zfMSd', :admin_level => 1}])
 Prompt.create([{:hero => 'Pirate with a Wooden Leg', :villain => 'Woodpecker', :goal => 'Float to Shore', :use_on => '20091017'}])
