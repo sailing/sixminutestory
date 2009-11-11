e79cec8c4bd2721ca75d64bcb0e523395144f31d
sailing <galen@sharearchy.com>
2009-11-11 21:43:38 +0900

    emails now sent from Six Minute Story

--- app/models/hermes.rb
+++ app/models/hermes.rb
@@ -5,28 +5,28 @@ class Hermes < ActionMailer::Base
 
 
   def comment_notification(writer, story, commenter, comment)
-     from "Six Minute Story <no-reply@sixminutestory.com>"
+     from "Six Minute Story no-reply@sixminutestory.com"
      recipients writer.email_address
      subject "Six Minute Story - #{commenter.login} commented on your story \"#{story.title}\""
      body :recipient => writer, :story => story, :commenter => commenter, :comment => comment
    end
    
    def signup_notification(recipient)
-       from "Six Minute Story <no-reply@sixminutestory.com>"
+     from "Six Minute Story no-reply@sixminutestory.com"
        recipients recipient.email_address
        subject "Six Minute Story - Welcome!"
        body :recipient => recipient
    end
    
    def following_notification(writer, follower)
-     from "Six Minute Story <no-reply@sixminutestory.com>"
+     from "Six Minute Story no-reply@sixminutestory.com"
       recipients writer.email_address
       subject "Six Minute Story - #{follower.login} is now following you!"
       body :recipient => writer, :follower => follower
    end
    
   def new_story_notification(followers, story, writer)
-    from "Six Minute Story <no-reply@sixminutestory.com>"
+    from "Six Minute Story no-reply@sixminutestory.com"
      recipients "no-reply@sixminutestory.com"
      bcc followers
      subject "Six Minute Story - #{writer.login} has written a new story!"
