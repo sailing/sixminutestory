91dd5596fc6dc18cb478dfe564cd3d2108a6bb61
sailing <galen@sharearchy.com>
2009-11-16 21:17:04 +0900

    moved toolbar

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -3,7 +3,7 @@ module StoriesHelper
   def approval_rating
     if @story.votes_count > 0
       if @story.votes_for > 0
-        pluralize(@story.rating, 'applauds', 'applaud')
+        pluralize(@story.rating, 'cheers', 'cheer')
       else
           result =  @story.votes_against.to_s + " shrugged"
       end
