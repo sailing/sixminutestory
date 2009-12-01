46459616c454df6210a180d5a5fae08954bd55d0
sailing <galen@sharearchy.com>
2009-12-02 06:11:21 +0900

    facebooker config mods for prod env

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,7 @@ ActionController::Routing::Routes.draw do |map|
     
     #user activities
     map.login 'login', :controller => "user_sessions", :action => "new"
+    map.login_fb 'login', :controller => "user_sessions", :action => "create", :conditions => { :method => :get }
     map.logout 'logout', :controller => "user_sessions", :action => "destroy"
     map.register 'register', :controller => "users", :action => "new"
     
