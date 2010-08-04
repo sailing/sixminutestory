2753fdd9d4b0f6f5b5cfb866a8cd7f7f955302b6
Galen <galenmail@gmail.com>
2010-08-04 01:39:16 -0700

    killed tags til can get them working, added counts to popular stories

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,12 +3,11 @@ class TagsController < ApplicationController
   def show
       page = params[:page] || 1
       per_page = 20
-#      offset = page > 1 ? per_page * page : 0
       offset = per_page
       order = "@relevance DESC"
       @tag = params[:name]
 
-#      @stories = Story.paginate :include => :taggings, :conditions => {:tag => @tag}, :page => page, :limit => per_page, :offset => offset, :finder => "find_tagged_with"
-    @stories = Story.search :page => page, :per_page => per_page, :order => order, :match_mode => :extended, :conditions => {:tag => @tag, :active => true}
+      @stories = Story.paginate :include => :taggings, :conditions => {:tag => @tag}, :page => page, :limit => per_page, :offset => offset, :finder => "find_tagged_with"
+
   end
 end
