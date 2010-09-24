4cb1f6e951b947b3727deaf06df20ab2e1eec470
Galen <galenmail@gmail.com>
2010-09-23 17:29:54 -0700

    turned on follower emails

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -4,7 +4,7 @@ class FollowingsController < ApplicationController
     if @following.save
       @user = User.find(params[:writer_id])
       
-      #Hermes.deliver_following_notification(@user, current_user) unless @user.send_followings == false
+      Hermes.deliver_following_notification(@user, current_user) unless (@user.send_followings == false or @user.email_address.blank?)
       
       respond_to do |format|
         format.html { 
