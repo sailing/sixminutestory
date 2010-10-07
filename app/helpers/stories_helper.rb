1287b1db624c608107e223a3822dc2591feb752b
Galen <galenmail@gmail.com>
2010-10-07 03:12:26 -0700

    changed terms to 'favorite' and derivatives. changed named scopes to more accurately reflect activity

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -3,12 +3,12 @@ module StoriesHelper
   def approval_rating
     if @story.votes_count > 0
       if @story.votes_for > 0
-        pluralize(@story.rating, 'cheer', 'cheers')
+        pluralize(@story.rating, 'favorites', 'favorites')
       else
           result =  @story.votes_against.to_s + " shrugged"
       end
     else
-      none = "No cheers"
+      none = "No favorites"
     end
   end
 end
