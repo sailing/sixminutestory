5a78ffd6d80b2710b934fdc48ffbb5ac316e48ab
Galen <galen@sixminutestory.com>
2011-06-13 02:06:13 -0700

    and removed migration again

--- db/migrate/20110506212254_add_karma_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddKarmaToUsers < ActiveRecord::Migration
-  def self.up
-		add_column :users, :reputation, :integer, :default => 0
-  end
-
-  def self.down
-		remove_column :users, :reputation
-  end
-end
