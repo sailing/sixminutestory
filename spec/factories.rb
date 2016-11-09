d69356fe70eb16787104c23f22cce1cdc2ba099f
Galen Sanford <galensanford@gmail.com>
2016-11-09 18:08:48 -0500

    story scaffold

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,2 +1,21 @@
 FactoryGirl.define do
+  factory :story do
+    title "MyString"
+    description "MyText"
+    license "MyString"
+    votes_count 1
+    comment_counter 1
+    flagged 1
+    counter 1
+    user_id 1
+    prompt_id 1
+    contest_id 1
+    delta false
+    active false
+    created_at "2016-11-09 17:59:26"
+    updated_at "2016-11-09 17:59:26"
+    cached_slug "MyString"
+    comments_count 1
+    featured false
+  end
 end
