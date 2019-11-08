9b93df22e682cd8d8dd4cfa98f986fc704dc676f
Galen Sanford <galen@sixminutestory.com>
2019-11-08 16:46:32 -0500

    add method to convert prompt licenses to human readable

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -4,6 +4,8 @@ class Prompt < ActiveRecord::Base
   has_one :contest
   acts_as_voteable
 
+  before_create :convert_license_to_human_terms
+
 	attr_accessor :hvg, :flickr, :startline
 
   scope :active, -> {where(:active => true)}
@@ -19,10 +21,8 @@ class Prompt < ActiveRecord::Base
   scope :hvg, lambda { active.where('kind = ? ', "hvg" ) }
   scope :firstlines, lambda { active.where('kind = ? ', "firstline" ) }
   scope :threewords, lambda { active.where('kind = ? ', "3ww" ) }
+  
   # Validations
-#  validates_presence_of   :hero, :message => "Need a hero. Sort of Important."
-#  validates_presence_of   :villain, :message => "Who's the villain?"
-#  validates_presence_of   :goal, :message => "You may not have goals, but heroes do."
   validates_uniqueness_of :use_on, :allow_blank => true
 
   FILTERS = [
@@ -32,54 +32,43 @@ class Prompt < ActiveRecord::Base
 
   ]
 
-#  {:scope => "popular", :label => "Popular"}
-# {:scope => "threewords", :label => "#3WW"}
-
-#  {:scope => "active",      :label => "Active"},
-#   {:scope => "inactive",    :label => "Inactive"},
-#   {:scope => "visible",     :label => "Visible"},
-#   {:scope => "invisible",   :label => "Not Visible"},
-#   {:scope => "high_rated",  :label => "High-Rated"},
-#   {:scope => "low_rated",   :label => "Low-Rated"}
-
-
 	def self.random
 	  active.where(id: Prompt.active.pluck(:id).sample).first
 	end
 
-	def license_in_human_terms(prompt)
-		if prompt.license.present? && prompt.license_url.blank?
-			case prompt.license
+	def convert_license_to_human_terms
+		if license.present? && license_url.blank?
+			case license
 				when "1", "cc-by-nc-sa"
-					prompt.license_en = "Attribution-NonCommercial-ShareAlike License"
-					prompt.license_url = "http://creativecommons.org/licenses/by-nc-sa/2.0/"
-					prompt.license_image_url = "http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png"
+					self.license_en = "Attribution-NonCommercial-ShareAlike License"
+					self.license_url = "http://creativecommons.org/licenses/by-nc-sa/2.0/"
+					self.license_image_url = "http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png"
 				when "2", "cc-by-nc"
-					prompt.license_en = "Attribution-NonCommercial License"
-					prompt.license_image_url = "http://i.creativecommons.org/l/by-nc/3.0/88x31.png"
-					prompt.license_url = "http://creativecommons.org/licenses/by-nc/2.0/"
+					self.license_en = "Attribution-NonCommercial License"
+					self.license_image_url = "http://i.creativecommons.org/l/by-nc/3.0/88x31.png"
+					self.license_url = "http://creativecommons.org/licenses/by-nc/2.0/"
 				when "3", "cc-by-nc-nd"
-					prompt.license_en = "Attribution-NonCommercial-NoDerivs License"
-					prompt.license_image_url = "http://i.creativecommons.org/l/by-nc-nd/3.0/88x31.png"
-					prompt.license_url = "http://creativecommons.org/licenses/by-nc-nd/2.0/"
+					self.license_en = "Attribution-NonCommercial-NoDerivs License"
+					self.license_image_url = "http://i.creativecommons.org/l/by-nc-nd/3.0/88x31.png"
+					self.license_url = "http://creativecommons.org/licenses/by-nc-nd/2.0/"
 				when "4", "cc-by"
-					prompt.license_en = "Attribution License"
-					prompt.license_url = "http://creativecommons.org/licenses/by/2.0/"
-					prompt.license_image_url = "http://i.creativecommons.org/l/by/3.0/88x31.png"
+					self.license_en = "Attribution License"
+					self.license_url = "http://creativecommons.org/licenses/by/2.0/"
+					self.license_image_url = "http://i.creativecommons.org/l/by/3.0/88x31.png"
 				when "5", "cc-by-sa"
-					prompt.license_en = "Attribution-ShareAlike License"
-					prompt.license_image_url = "http://i.creativecommons.org/l/by-sa/3.0/88x31.png"
-					prompt.license_url = "http://creativecommons.org/licenses/by-sa/2.0/"
+					self.license_en = "Attribution-ShareAlike License"
+					self.license_image_url = "http://i.creativecommons.org/l/by-sa/3.0/88x31.png"
+					self.license_url = "http://creativecommons.org/licenses/by-sa/2.0/"
 				when "6", "cc-by-nd"
-					prompt.license_en = "Attribution-NoDerivs License"
-					prompt.license_image_url = "http://i.creativecommons.org/l/by-nd/3.0/88x31.png"
-					prompt.license_url = "http://creativecommons.org/licenses/by-nd/2.0/"
+					self.license_en = "Attribution-NoDerivs License"
+					self.license_image_url = "http://i.creativecommons.org/l/by-nd/3.0/88x31.png"
+					self.license_url = "http://creativecommons.org/licenses/by-nd/2.0/"
 				else
-					prompt.license_en = "No known copyright restrictions"
-					prompt.license_url = "http://flickr.com/commons/usage/"
+					self.license_en = "No known copyright restrictions"
+					self.license_url = "http://flickr.com/commons/usage/"
 			end
-			prompt.save
-			return prompt
+			self.save
+			return self
 		end
 	end
 end
