2a6923e871ed1a069d43b8f173e8a99bb868fe55
Galen Sanford <galen@sixminutestory.com>
2019-12-06 12:12:12 -0500

    auto login after confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Sms::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'users/confirmations' }
   
   resources :followings
 
