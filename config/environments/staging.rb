ab7890eb88cc200686ef2733cd740ab90d027c64
Galen Sanford <galen@sixminutestory.com>
2019-12-06 23:30:31 -0500

    update staging env file

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -40,6 +40,8 @@ Sms::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
+  config.assets.gzip = true
+  config.assets.js_compressor  = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   # config.assets.compile = false
